39664d14e5c1a68497e107aacddd95e2a78928ba
abusaadkhan <abu70179@gmail.com>
2023-07-01 17:43:49 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { createContext, useContext,useState } from 'react';
+import Timer from './component/Timer';
+import Settings from './component/Settings';
+
+export  const timerContext = createContext()
+   
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(25);
+  const [breakMinutes, setBreakMinutes] = useState(5);
+  const [cycle,setCycle] = useState(2)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-[#30384b] text-white  text-center flex flex-col justify-center items-center w-full  h-screen' >
+      <timerContext.Provider value={{
+        showSettings,
+        workMinutes,
+        breakMinutes,
+        cycle,
+        setShowSettings,
+        setCycle,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </timerContext.Provider>
+    
     </div>
   );
 }
---
+++ src/component/Settings.js
@@ -0,0 +1,56 @@
+import ReactSlider from 'react-slider';
+
+import { timerContext } from '../App';
+import {useContext} from "react";
+
+
+
+function Settings() {
+  const settingsInfo = useContext(timerContext);
+  return(
+    <div className='text-left w-[40%]  flex flex-col items-start'>
+      <label className='pb-4' >work: {settingsInfo.workMinutes}:00</label>
+      <ReactSlider
+        className='h-10 w-full rounded-[20px] border-2 border-solid border-[#f54e4e]  '
+        thumbClassName='bg-[#f54e4e] w-9 h-9 rounded-full border-none '
+        trackClassName='border-none'
+        value={settingsInfo.workMinutes}
+        onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
+        min={1}
+        max={60}
+      />
+      <label className='pt-8 pb-4'>break: {settingsInfo.breakMinutes}:00</label>
+      <ReactSlider
+        className='h-10 w-full rounded-[20px] border-2 border-solid border-[#4aec8c]  '
+        thumbClassName='bg-[#4aec8c] w-9 h-9 rounded-full border-none '
+        trackClassName={'track'}
+        value={settingsInfo.breakMinutes}
+        onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
+        min={1}
+        max={60}
+      />
+      <label className='pt-8 pb-4'>Cycle: {settingsInfo.cycle}</label>
+      <ReactSlider
+        className='h-10 w-full rounded-[20px] border-2 border-solid border-[#4a9eec]  '
+        thumbClassName='bg-[#4a9eec] w-9 h-9 rounded-full border-none '
+        trackClassName={'track'}
+        value={settingsInfo.cycle}
+        onChange={newValue => settingsInfo.setCycle(newValue)}
+        min={1}
+        max={100}
+      />
+      <div className='text-center mt-5 w-full flex justify-center  '>
+       
+        <button className='w-auto rounded-lg py-2 px-4 text-xl ' onClick={() => settingsInfo.setShowSettings(false)}>
+            <svg xmlns="http://www.w3.org/2000/svg" className=" w-9 " viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z" clipRule="evenodd" />
+            </svg>
+            Back
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Settings;
